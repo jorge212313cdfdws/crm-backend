15ac702d28295147c18cec0ad66b3bd9898b8f6c
jorge212313cdfdws <jorgedelaparra0@gmail.com>
2026-03-04 14:04:58 +0100

    primer commit

---
+++ crm-backend/src/main/java/com/crm/repository/ClientePerfilRepository.java
@@ -0,0 +1,7 @@
+package com.crm.repository;
+
+import com.crm.entity.ClientePerfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientePerfilRepository extends JpaRepository<ClientePerfil, Integer> {
+}
