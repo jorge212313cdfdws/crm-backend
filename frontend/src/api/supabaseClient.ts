f590459a87787e57652a060d3614721b3531fd88
MaximoMezzoni <maximomezzoni@gmail.com>
2026-03-18 10:57:38 +0000

    feat: crear usuario en Supabase Auth al añadir empleado desde el ERP

--- frontend/src/api/supabaseClient.ts
+++ frontend/src/api/supabaseClient.ts
@@ -5,4 +5,9 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 );
 
+export const supabaseAdmin = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY
+);
+
 export default supabase;
